1dd8981b39d3549c9f18703df53eca7a2f97eb39
kalaiselvan <kalaiselvn511@gmail.com>
2025-10-29 06:36:45 -0700

    day19
    js

---
+++ Day19/task6.js
@@ -0,0 +1,4 @@
+let tab = 5;
+for (let j = 1; j <= 10; j++)
+    console.log(tab + "X" +j +"="+(tab*j));
+    
---
+++ Day19/task9.js
@@ -0,0 +1,6 @@
+let limit = prompt("Enter a number:");
+limit = Number(limit);
+
+for (let i = 1; i <= limit; i++) {
+  console.log(i);
+}
