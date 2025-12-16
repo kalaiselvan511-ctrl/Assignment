d2d97354f358e1fa3e3f037b8ab3786c9750e8d6
karnan-2003 <144413056+karnan-2003@users.noreply.github.com>
2025-12-16 20:03:15 +0530

    day45
    react

---
+++ Day45/src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+import GreetingInput from './Day45/task01'
+import UppercaseInput from './Day45/task02'
+import ControlledCheckbox from './Day45/task03'
+import CountrySelect  from './Day45/task04'
+import PasswordMatch from './Day45/task05'
+import LoginForm from './Day45/task06'
+import ProfileUpdateForm from './Day45/task08'
+import ValidatedForm from './Day45/task09'
+import FeedbackForm from './Day45/task10'
+
+
+function App() {
+
+
+  return (
+    <>
+    <h1>KALAI SELVAN</h1>
+    <GreetingInput/>
+    <hr />
+    <UppercaseInput/>
+    <hr />
+    <ControlledCheckbox/>
+    <hr />
+    <CountrySelect/>
+    <hr />
+    <PasswordMatch/>
+    <hr />
+    <LoginForm/>
+    <hr />
+    <ProfileUpdateForm/>
+    <hr />
+    <ValidatedForm/>
+    <hr />
+    <FeedbackForm/>
+  
+    </>
+  )
+}
+
+export default App
---
+++ Day45/src/Day45/task04.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function CountrySelect() {
+  const [country, setCountry] = useState("");
+
+  return (
+    <div>
+      <select
+        value={country}
+        onChange={(e) => setCountry(e.target.value)}
+      >
+        <option value="" disabled>
+          Select a country
+        </option>
+        <option value="USA">United States</option>
+        <option value="CAN">Canada</option>
+        <option value="UK">United Kingdom</option>
+        <option value="AUS">Australia</option>
+      </select>
+
+      <p>Selected country: {country || "None"}</p>
+    </div>
+  );
+}
+
+export default CountrySelect;
